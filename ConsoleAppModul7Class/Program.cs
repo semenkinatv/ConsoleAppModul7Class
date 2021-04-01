0d262d3334cca195d0415260f11ec200d38e1f2f
semenkinatv <semenkinatv@gmail.com>
2021-04-01 21:04:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppModul7Class/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppModul7Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
